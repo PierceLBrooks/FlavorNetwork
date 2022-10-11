fb25de54ba461910c26712d6c8e9d9015b7cd6e7
Pierce Brooks <piercebrooks@rocketmail.com>
2022-10-11 13:36:30 -0400

    introduce usage of hnsrtree dependency

--- Project/flavornet/main.cpp
+++ Project/flavornet/main.cpp
@@ -1,18 +1,44 @@
 
+#include <string>
+#include <cstring>
+#include <cstdio>
+#include <cstdlib>
 #include <iostream>
 #include "flavornet.h"
+#include "HnSRTree/HnSRTreeFile.hh"
+#include "HnSRTree/HnPoint.hh"
+#include "HnSRTree/HnDataItem.hh"
 
 int main(int argc, char** argv)
 {
-    std::cout << "Hello, world!" << std::endl;
-    std::cout << nameCount << std::endl;
-    std::cout << vectorSize << std::endl;
-    std::cout << ingredientNames[nameCount-1] << std::endl;
-    for (int i = 0; i < vectorSize; i++)
+    HnSRTreeFile indexFile;
+    HnPointVector points = new_HnPointVector();
+    HnDataItemVector dataItems = new_HnDataItemVector();
+    HnProperties properties = new_HnProperties();
+    int dataItemSize = 0;
+    for (int i = 0; i < nameCount; i++)
     {
-        std::cout << ingredientVectors[nameCount-1][i];
+        int length = strlen(ingredientNames[i])+1;
+        HnPoint point = new_HnPoint(vectorSize);
+        HnDataItem dataItem = new_HnDataItem(ingredientNames[i], length);
+        if (length > dataItemSize)
+        {
+            dataItemSize = length;
+        }
+        for (int j = 0; j < vectorSize; j++)
+        {
+            point.setCoordAt((double)ingredientVectors[i][j], j);
+        }
+        points.addElement(point);
+        dataItems.addElement(dataItem);
     }
-    std::cout << std::endl;
+    dataItemSize += 1;
+    indexFile = new_HnSRTreeFile("flavornet.idx", vectorSize, dataItemSize, points, dataItems, properties);
+    if (indexFile == HnSRTreeFile::null)
+    {
+        return -1;
+    }
+    indexFile.close();
     return 0;
 }
 
